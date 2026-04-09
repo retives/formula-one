84e0d05cede9e69ad7dc15300214425314d072d1
srgtkrvvv <tr3e1t0ry@gmail.com>
2026-04-09 12:59:42 +0300

    Add bronze tables

--- src/formula_one_etl/transformations/bronze/passive_data.py
+++ src/formula_one_etl/transformations/bronze/passive_data.py
@@ -1,5 +1,38 @@
 from pyspark import pipelines as dp
+from formula_one_etl.transformations.utils.schemas import *
+from formula_one_etl.transformations.utils.api_requsts import *
 
 @dp.table(
-    name = "bronze_driver_championship"
+    name="bronze_meetings",
+    schema=meetings_schema
 )
+def bronze_meetings():
+    return ingest_data(ENDPOINTS['meetings'])
+
+@dp.table(
+    name="bronze_sessions",
+    schema=sessions_schema
+)
+def bronze_sessions():
+    return ingest_data(ENDPOINTS['sessions'])
+
+@dp.table(
+    name="bronze_drivers",
+    schema=drivers_schema
+)
+def bronze_drivers():
+    return ingest_data(ENDPOINTS['drivers'])
+
+@dp.table(
+    name="bronze_session_results",
+    schema=session_results_schema
+)
+def bronze_session_results():
+    return ingest_data(ENDPOINTS['session_result'])
+
+@dp.table(
+    name="bronze_starting_grid",
+    schema=starting_grid_schema
+)
+def bronze_starting_grid():
+    return ingest_data(ENDPOINTS['starting_grid'])
---
+++ src/formula_one_etl/transformations/utils/api_requsts.py
@@ -0,0 +1,22 @@
+import requests
+BASE_URL = "https://api.openf1.org/v1"
+ENDPOINTS = {
+    "meetings": f"{BASE_URL}/meetings",
+    "championship_drivers": f"{BASE_URL}/championship_drivers",
+    "sessions": f"{BASE_URL}/sessions",
+    "championship_teams": f"{BASE_URL}/championship_teams",
+    "drivers": f"{BASE_URL}/drivers",
+    "session_result": f"{BASE_URL}/session_result",
+    "starting_grid": f"{BASE_URL}/starting_grid",
+
+}
+
+def ingest_data(url:str):
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Error occurred, data won't be loaded correctly. Status code - {response.status_code}")
+
+    data = response.json()
+    df = spark.createDataFramee(data)
+    return df
