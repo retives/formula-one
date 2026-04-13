a34a92b00a8d1a710f3fd3d67b4e0f44665394d5
retives <srgtkrvvv@proton.me>
2026-04-13 07:05:50 +0000

    Add active tables

---
+++ src/formula_one_etl/transformations/bronze/active_data.py
@@ -0,0 +1,48 @@
+from pyspark import pipelines as dp
+from transformations.utils.api_requests import *
+from transformations.utils.schemas import *
+@dp.table(
+    name="bronze_laps_duration",
+    schema=laps_schema
+)
+def bronze_laps_duration():
+    return spark.read.format("json").load("/Volumes/dbr_dev/tokariev_bronze/openf1_data/laps.json")
+
+@dp.table(
+    name="bronze_overtakes",
+    schema=overtakes_schema
+)
+def bronze_overtakes():
+    return spark.read.format("json").load("/Volumes/dbr_dev/tokariev_bronze/openf1_data/overtakes.json")
+
+@dp.table(
+    name="bronze_pit_stops",
+    schema=pit_stop_schema
+)
+def bronze_pit():
+    return spark.read.format("json").load("/Volumes/dbr_dev/tokariev_bronze/openf1_data/pit.json")
+
+
+@dp.table(
+    name="bronze_weather",
+    schema=weather_schema
+)
+def bronze_weather():
+    return spark.read.format("json").load("/Volumes/dbr_dev/tokariev_bronze/openf1_data/weather.json")
+
+@dp.table(
+    name="bronze_race_control",
+    schema=race_control_schema
+)
+def bronze_race_control():
+    return spark.read.format("json").load("/Volumes/dbr_dev/tokariev_bronze/openf1_data/race_control.json")
+
+@dp.table(
+    name="bronze_position",
+    schema=position_schema
+)
+def bronze_():
+    return spark.read.format("json").load("/Volumes/dbr_dev/tokariev_bronze/openf1_data/position.json")
+
+
+
